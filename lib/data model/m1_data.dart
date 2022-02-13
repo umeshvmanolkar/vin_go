0b5dba8d2ec8f054f03d102ebbb57940182341dd
Umesh <umeshvmanolkar@gmail.com>
2022-02-13 13:14:40 +0530

    M1 completed.

---
+++ lib/data model/m1_data.dart
@@ -0,0 +1,6 @@
+m1Data(int vinCount, int delay, String different) async{
+  Map data = {
+    'vinCount':vinCount,
+    'delay':delay,
+    'different': different
+  };}
--- lib/m1.dart
+++ lib/m1.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class M1 extends StatefulWidget {
   const M1({Key? key}) : super(key: key);
@@ -8,73 +11,114 @@ class M1 extends StatefulWidget {
 }
 
 class _M1State extends State<M1> {
-  TextEditingController vinController = TextEditingController();
-  TextEditingController countController = TextEditingController();
+
+
+  TextEditingController vinCountController = TextEditingController();
+  TextEditingController delayController = TextEditingController();
   TextEditingController sameVinController = TextEditingController();
+
+
+  postData(String vinCount,String delay, String different) async{
+    Map data = {
+      'vinCount':vinCount,
+      'delay':delay,
+      'different':different
+    };
+
+    final response = await http.post(Uri.parse('http://192.168.29.149:8082/data'),body: data);  //API response
+
+    if(response.statusCode == 200){            //checking status of API
+
+      var jsonData = json.decode(response.body);
+
+      if(jsonData['Success']=="Success!"){                                    //validating user login credentials
+          setState(() {
+
+          String jsonsDataString = response.body.toString(); // toString of Response's body is assigned to jsonDataString
+          data = jsonDecode(jsonsDataString);
+
+        });
+
+      } else {
+
+      }
+    } else{
+
+    }
+  }
+
+  final GlobalKey<FormState> _formKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body:Container(alignment: Alignment.center,
           child: SingleChildScrollView(
-            child: Column(
-                crossAxisAlignment:CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children:[
+            child: Form(
+              key: _formKey,
+              child: Column(
+                  crossAxisAlignment:CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children:[
 
-                  Text("M1", style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
+                    Text("M1", style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
 
-                  Container(
-                    width: 300,
-                    child: TextField(
-                      controller: vinController,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          filled: true,
-                          hintStyle: TextStyle(color: Colors.grey[800]),
-                          hintText: "Enter VIN",
-                          fillColor: Colors.white70),
+                    Container(
+                      width: 300,
+                      child: TextField(
+                        controller: vinCountController,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                            filled: true,
+                            hintStyle: TextStyle(color: Colors.grey[800]),
+                            hintText: "Enter VIN",
+                            fillColor: Colors.white70),
+                      ),
                     ),
-                  ),
 
-                  Container(
-                    width: 300,
-                    child: TextField(
-                      controller: countController,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          filled: true,
-                          hintStyle: TextStyle(color: Colors.grey[800]),
-                          hintText: "Enter Speed",
-                          fillColor: Colors.white70),
+                    Container(
+                      width: 300,
+                      child: TextField(
+                        controller: delayController,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                            filled: true,
+                            hintStyle: TextStyle(color: Colors.grey[800]),
+                            hintText: "Enter Speed",
+                            fillColor: Colors.white70),
+                      ),
                     ),
-                  ),
 
-                  Container(
-                    width: 300,
-                    child: TextField(
-                      controller: sameVinController,
-                      decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10.0),
-                          ),
-                          filled: true,
-                          hintStyle: TextStyle(color: Colors.grey[800]),
-                          hintText: "Type 'Y' or 'N'",
-                          fillColor: Colors.white70),
+                    Container(
+                      width: 300,
+                      child: TextField(
+                        controller: sameVinController,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                            filled: true,
+                            hintStyle: TextStyle(color: Colors.grey[800]),
+                            hintText: "Type 'Y' or 'N'",
+                            fillColor: Colors.white70),
+                      ),
                     ),
-                  ),
 
-                  RaisedButton(
-                      child: Text("Submit"),
-                      onPressed:(){
+                    RaisedButton(
+                        child: Text("Submit"),
+                        onPressed:() async{
+                          print(vinCountController.text);
+                          print(postData(vinCountController.text, delayController.text, sameVinController.text));
 
-                      }
-                  ),
-                ]),
+
+                        }
+                    ),
+                  ]),
+            ),
           )),
     );
   }
